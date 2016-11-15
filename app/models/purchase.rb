189282b5c2472026865b56a8c13820090ac3f3f4
ondenyieric <eric.ondenyi@meltwater.org>
2016-11-15 12:38:53 +0000

    Cleaned off purchase scaffolds that were accidentally created on project

--- app/models/purchase.rb
+++
@@ -1,4 +0,0 @@
-class Purchase < ActiveRecord::Base
-validates :name, presence: true
-validates :cost, numericality: {greater_than: 0 }
-end
--- app/views/purchases/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @purchase, :id, :name, :cost, :created_at, :updated_at
